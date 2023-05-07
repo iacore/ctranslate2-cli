d76231a7dd0daa3f629617b188ad7e41e629963f
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-05-07 01:54:20 +0000

    Create simple cli

--- cli.py
+++ cli.py
@@ -1,17 +1,43 @@
-import sys, pathlib
+import sys, pathlib, argparse
 from api import Translator
 
-basedir = pathlib.Path('~/.local/share/argos-translate/packages').expanduser()
-t = Translator(basedir / "en_de")
+basedir = pathlib.Path("models")
 
-while True:
-    to_translate = ""
+
+def main(modeldir):
+    t = Translator(modeldir)
 
     while True:
-        line = sys.stdin.readline()
-        if len(line) == 0:
-            break
-        to_translate += line
-    
-    for guess in t.translate(to_translate):
-        print(guess)
+        print(">>>")
+        to_translate = ""
+
+        while True:
+            line = sys.stdin.readline()
+            if len(line) == 0:
+                break
+            to_translate += line
+
+        for guess in t.translate(to_translate):
+            print("<<<")
+            print(guess)
+
+
+def fail(s):
+    print(s, file=sys.stderr)
+    exit(1)
+
+
+parser = argparse.ArgumentParser(
+    description="CLI for Argos Translate",
+)
+parser.add_argument('lang', help="pair of language codes, e.g. en_ar")
+args = parser.parse_args()
+
+modeldir = basedir / args.lang
+
+if not modeldir.exists():
+    fail(f"No directory: {modeldir}\nlang={args.lang}")
+try:
+    main(modeldir=modeldir)
+except KeyboardInterrupt:
+    pass
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+This is a cli application for Argos Translate.
+
 ## Download Models
 
 Download from [Direct Link/IPFS: JSON](https://github.com/argosopentech/argospm-index/blob/main/index.json). Also see [Web Index](https://www.argosopentech.com/argospm/index/).
@@ -6,4 +8,15 @@ The file extension is `.argosmodel` but it's a Zip file. Extract it under for ex
 
 ## Usage
 
-TK
+```
+pip install ctranslate2 sentencepiece
+python cli.py
+```
+
+After entering text, press Ctrl+D on an **empty line** to submit.
+
+Press Ctrl+C to exit.
+
+## TODO
+
+Python is too heavy weight for this. Can swap it out later.
